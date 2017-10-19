19964f2c99eef0a41fb48c0ab1d1153a779fac26
evoliofly <antoine.murat@protonmail.com>
2017-10-19 22:28:42 +0200

    HTTPS

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const Bot = require('./classes/Bot')
 const appToken = 'EAAadBOjD6PcBALqe46IdeOhOCsJK1oAwiH369GKbTcrrzFDSFZA4QI1ZCKJdwtNus4cgsrdfCGfm4LPOhJDHEeZB9CWbL0TSGSJD0I4JZAcPsnLZBNqxVfXV4O9GryZCF5nViWMB65AfEEeWZBMxvTIWpH8xefymNj2LXvmTQ6Vi8ZAO4lye7g0w'
 const verifyToken = 'wellhooked'
 
-httpServer = new HTTPServer(80, './static')
+httpServer = new HTTPServer(80, 443, './static')
 bot = new Bot(httpServer, appToken, verifyToken)
 
 setInterval(_ => bot.notifyAll(), 5000)
--- classes/HTTPServer.js
+++ classes/HTTPServer.js
@@ -1,17 +1,20 @@
+const https = require('https')
 const express = require('express')
+
+const options = {
+    cert: fs.readFileSync('./sslcert/fullchain.pem'),
+    key: fs.readFileSync('./sslcert/privkey.pem')
+}
 
 class HTTPServer{
 
-	constructor(port = 80, staticDirectory = './../static/'){
+	constructor(port = 80, httpsPort = 443, staticDirectory = './../static/'){
 
 		this.app = express()
 		this.app.use(express.static(staticDirectory))
 
-		
-
-
 		this.app.listen(port, _ => console.log(`Serveur web en écoute sur le port ${port}.`))
-
+		https.createServer(options, this.app).listen(httpsPort, , _ => console.log(`Serveur web sécurisé en écoute sur le port ${httpsPort}.`))
 	}
 
 }
