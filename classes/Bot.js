b26283eaa59105fafae5ff9ed951ce101e379147
Antoine Murat <antoine.murat@nsigma.fr>
2017-12-29 11:00:33 +0100

    debug google maps

--- classes/Bot.js
+++ classes/Bot.js
@@ -207,7 +207,7 @@ class Bot{
 
 		const buttons =  [{
 			type: "phone_number",
-			title: `Appeler ${sos.coordonnees.numero}`,
+			title: `Appeler ${sos.coordonnees.phone}`,
 			payload: sos.coordonnees.numero,
 		}]
 
@@ -239,6 +239,8 @@ class Bot{
 
 		const lat = sos.coordonnees.coordinates.lat,
 					lng = sos.coordonnees.coordinates.lng
+
+		console.log(`https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=13&size=400x200&maptype=roadmap&markers=color:red%7C${lat},${lng}&key=${MAPS_API_KEY}`)
 
 		return {
 			title: sos.type,
