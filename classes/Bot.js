9d825a521c313c5ca68d04ab81c2793557a5aa8b
Antoine Murat <antoine.murat@nsigma.fr>
2017-12-29 03:29:27 +0100

    test sans image

--- classes/Bot.js
+++ classes/Bot.js
@@ -244,9 +244,9 @@ class Bot{
 			title: sos.type,
 			subtitle: details,
 			// item_url: "https://www.oculus.com/en-us/rift/",
-			image_url: `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=13&size=400x200&maptype=roadmap
+			/*image_url: `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=13&size=400x200&maptype=roadmap
 				&markers=color:red%7C${lat},${lng}
-				&key=${MAPS_API_KEY}`,
+				&key=${MAPS_API_KEY}`,*/
 			buttons: buttons
 		}
 	}
